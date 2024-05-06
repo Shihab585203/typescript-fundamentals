ce585bf6a6a8a81ea360dc38cd86c0e1adb68be1
Shihab585203 <freelancersehab1@gmail.com>
2024-05-06 19:54:52 +0600

    second

--- src/module 3/3.4.ts
+++ src/module 3/3.4.ts
@@ -1,53 +1,61 @@
 {
-    //instance of guard
-    class Animal {
-        name: string;
-        species: string;
+  //instance of guard
+  class Animal {
+    name: string;
+    species: string;
 
-
-        constructor ( name: string, species: string){
-            this.name = name;
-            this. species = species;
-        }
-
-        makeSound(){
-            console.log("Don't make a sound");
-        }
+    constructor(name: string, species: string) {
+      this.name = name;
+      this.species = species;
     }
 
-    class Dog extends Animal{
-        constructor(name: string, species: string){
-            super(name, species)
-        }
-        makeBark(){
-            console.log("I am barking.");
-        }
+    makeSound() {
+      console.log("Don't make a sound");
     }
+  }
 
-    class Cat extends Animal{
-        constructor(name: string, species: string){
-            super(name, species)
-        }
-        makeMeaw(){
-            console.log("I keep doing Meaw.");
-        }
+  class Dog extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
     }
-
-    const getAnimal = (animal: Animal) => {
-        if(animal instanceof Dog){
-
-            animal.makeBark();
-        } else if ( animal instanceof Cat){
-            animal.makeMeaw();
-        } else {
-            animal.makeSound();
-        }
+    makeBark() {
+      console.log("I am barking.");
     }
+  }
 
-    const dog = new Dog("Purple", "Dog");
-    const cat = new Cat("Pinky", "Cat");
+  class Cat extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species);
+    }
+    makeMeaw() {
+      console.log("I am keeping doing Meaw.");
+    }
+  }
 
-    
+  // Use function with smart way to handle instanceof
 
-    //
+  const isDog = (animal: Animal): animal is Dog => {
+    return animal instanceof Dog;
+  };
+
+  const isCat = (animal: Animal): animal is Cat => {
+    return animal instanceof Cat;
+  };
+
+  const getAnimal = (animal: Animal) => {
+    if (isDog(animal)) {
+      animal.makeBark();
+    } else if (isCat(animal)) {
+      animal.makeMeaw();
+    } else {
+      animal.makeSound();
+    }
+  };
+
+  const dog = new Dog("Purple", "Dog");
+  const cat = new Cat("Pinky", "Cat");
+
+  getAnimal(cat);
+
+  //
 }
